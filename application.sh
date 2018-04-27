9292c19dbe69af112a571d5829c3d116be51c2da
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-04-27 22:26:38 +0530

    Create application.sh

---
+++ application.sh
@@ -0,0 +1,10 @@
+yum install httpd - y
+if [ -a /usr/sbin/httpd ]; then'
+echo "Apache is already installed"
+else
+sudo yum install httpd -y
+fi
+sudo rm -rf /var/www/html/*
+touch /var/www/html/index.html
+echo - e "<html> \n <header><title>This is title</title></header> \n <body> \n Hello World \n </body> \n </html>" >> /var/www/html/index.html
+service httpd restart
